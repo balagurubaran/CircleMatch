64ba718ad935bd06e3d6b1a34f8f15efe3c94be5
balagurubaran <momofila@momofilas-MacBook-Pro.local>
2015-05-25 09:54:38 +0530

    Help text issue
    Help text issue

--- FirstGame/HelpScene.m
+++ FirstGame/HelpScene.m
@@ -12,15 +12,49 @@
 @implementation HelpScene
 
 -(void)didMoveToView:(SKView *)view {
-    SKLabelNode *helpTextNode = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue "];
-    helpTextNode.text = @"Pair same colour, same size cirlce with in the time frame";
-    helpTextNode.fontSize = 40;
-    helpTextNode.fontColor = [SKColor blackColor];
-    helpTextNode.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-    [self addChild:helpTextNode];
+    [self multiLine:@"Pair same colour, same size cirlce with in the time frame"];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     [[NSNotificationCenter defaultCenter] postNotificationName:@"addMenuScene" object:nil];
+}
+
+-  (void) multiLine:(NSString*)labelText{
+    NSCharacterSet *separators = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    NSArray *words = [labelText componentsSeparatedByCharactersInSet:separators];
+    
+    int len = [labelText length];
+    int width = self.frame.size.width - 50; // specify your own width to fit the device screen
+    
+    // get the number of labelnode we need.
+    int totLines = len/width + 1;
+    int cnt = 0; // used to parse through the words array
+    
+    // here is the for loop that create all the SKLabelNode that we need to
+    // display the string.
+    
+    for (int i=0; i < totLines; i++) {
+        int lenPerLine = 0;
+        NSString *lineStr = @"";
+        
+        while (lenPerLine<width) {
+            if (cnt>[words count]-1) break; // failsafe - avoid overflow error
+            lineStr = [NSString stringWithFormat:@"%@ %@", lineStr, words[cnt]];
+            lenPerLine = [lineStr length];
+            cnt ++;
+            // NSLog(@"%@", lineStr);
+        }
+        // creation of the SKLabelNode itself
+        SKLabelNode *_multiLineLabel = [SKLabelNode labelNodeWithFontNamed:@"Oxygen Light"];
+        _multiLineLabel.text = lineStr;
+        // name each label node so you can animate it if u wish
+        // the rest of the code should be self-explanatory
+        _multiLineLabel.name = [NSString stringWithFormat:@"line%d",i];
+        _multiLineLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentModeCenter;
+        _multiLineLabel.fontSize = 35;
+        _multiLineLabel.fontColor = [SKColor blackColor];
+        _multiLineLabel.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+        [self addChild:_multiLineLabel];
+    }
 }
 @end
